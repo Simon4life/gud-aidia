7681eea7d970ab71ed3eba523b4f81e38bd75b3b
Ogar Simon <ogarsimon0000@gmail.com>
2025-06-10 06:01:45 -0700

    initial commit

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -18,7 +18,7 @@ const Hero = ({header, subheader, links}: heroInterface) => {
           <p className="text-lg text-center md:text-xl mb-8">{subheader}</p>
 
           {
-            links && (<div className="flex gap-8 justify-center items-center font-semibold">
+            links && (<div className="flex flex-col md:flex-row gap-8 justify-center items-center font-semibold">
               <Link href="/services" className="px-5 py-6  rounded flex items-center justify-center text-black bg-purple-300 hover:bg-yellow-100">Get Started <span className="ml-4"><MdArrowOutward/></span></Link>
               <Link href="/campaign" className="border-b inline-block border-white text-white py-2 hover:text-yellow-600 transition">Join the Campaign</Link>
             </div>)
